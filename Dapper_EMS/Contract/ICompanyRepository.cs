11798c73697f9a660dfb0654d976dca3b09dd048
Agiratech-Kiruthika <kiruthika.p@agiratech.com>
2024-02-13 16:34:48 +0530

    Add project files.

---
+++ Dapper_EMS/Contract/ICompanyRepository.cs
@@ -0,0 +1,10 @@
+﻿using Dapper_EMS.Entities;
+
+namespace Dapper_EMS.Contract
+{
+    public interface ICompanyRepository
+    {
+        public Task<IEnumerable<Company>> GetCompanies();
+
+    }
+}
---
+++ Dapper_EMS/Controllers/CompaniesController.cs
@@ -0,0 +1,37 @@
+﻿using Dapper_EMS.Contract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dapper_EMS.Controllers
+{
+    //[Route("api/companies")]
+    //[ApiController]
+    public class CompaniesController : Controller
+    {
+        private readonly ICompanyRepository _companyRepo;
+
+        public CompaniesController(ICompanyRepository companyRepo)
+        {
+            _companyRepo = companyRepo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCompanies()
+        {
+            try
+            {
+                var companies = await _companyRepo.GetCompanies();
+                return Ok(companies);
+            }
+            catch (Exception ex)
+            {
+                //log error
+                return StatusCode(500, ex.Message);
+            }
+        }
+        public async Task<IActionResult> Index()
+        {
+            var companies = await _companyRepo.GetCompanies();
+            return View(companies);
+        }
+    }
+}
