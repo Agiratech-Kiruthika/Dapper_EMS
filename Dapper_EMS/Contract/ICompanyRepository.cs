b452392a34804a384a3b2ccd7d107e62325ef6e5
Agiratech-Kiruthika <kiruthika.p@agiratech.com>
2024-02-16 18:40:52 +0530

    Implementing the HTTP method

--- Dapper_EMS/Contract/ICompanyRepository.cs
+++ Dapper_EMS/Contract/ICompanyRepository.cs
@@ -1,10 +1,17 @@
-﻿using Dapper_EMS.Entities;
+﻿using Dapper_EMS.Dto;
+using Dapper_EMS.Entities;
 
 namespace Dapper_EMS.Contract
 {
     public interface ICompanyRepository
     {
         public Task<IEnumerable<Company>> GetCompanies();
+        public Task<Company> GetCompany(int id);
+
+        public Task<Company> CreateCompany(CompanyForCreationDto company);
+
+        public Task UpdateCompany(int id, CompanyForUpdateDto company);
+        public Task DeleteCompany(int id);
 
     }
 }
--- Dapper_EMS/Controllers/CompaniesController.cs
+++ Dapper_EMS/Controllers/CompaniesController.cs
@@ -1,10 +1,11 @@
 ﻿using Dapper_EMS.Contract;
+using Dapper_EMS.Dto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Dapper_EMS.Controllers
 {
-    //[Route("api/companies")]
-    //[ApiController]
+    [Route("api/companies")]
+    [ApiController]
     public class CompaniesController : Controller
     {
         private readonly ICompanyRepository _companyRepo;
@@ -20,7 +21,7 @@ namespace Dapper_EMS.Controllers
             try
             {
                 var companies = await _companyRepo.GetCompanies();
-                return Ok(companies);
+                return Json(companies);
             }
             catch (Exception ex)
             {
@@ -28,10 +29,81 @@ namespace Dapper_EMS.Controllers
                 return StatusCode(500, ex.Message);
             }
         }
-        public async Task<IActionResult> Index()
+        //public async Task<IActionResult> Index()
+        //{
+        //    var companies = await _companyRepo.GetCompanies();
+        //    return View(companies);
+        //}
+
+        [HttpGet("{id}", Name = "CompanyById")]
+        public async Task<IActionResult> GetCompany(int id)
         {
-            var companies = await _companyRepo.GetCompanies();
-            return View(companies);
+            try
+            {
+                var company = await _companyRepo.GetCompany(id);
+                if (company == null)
+                    return NotFound();
+
+                return Ok(company);
+            }
+            catch (Exception ex)
+            {
+                //log error
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCompany([FromBody]CompanyForCreationDto company)
+        {
+            try
+            {
+                var createdCompany = await _companyRepo.CreateCompany(company);
+                return CreatedAtRoute("CompanyById", new { id = createdCompany.Id }, createdCompany);
+            }
+            catch (Exception ex)
+            {
+                //log error
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCompany(int id, CompanyForUpdateDto company)
+        {
+            try
+            {
+                var dbCompany = await _companyRepo.GetCompany(id);
+                if (dbCompany == null)
+                    return NotFound();
+
+                await _companyRepo.UpdateCompany(id, company);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                //log error
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCompany(int id)
+        {
+            try
+            {
+                var dbCompany = await _companyRepo.GetCompany(id);
+                if (dbCompany == null)
+                    return NotFound();
+
+                await _companyRepo.DeleteCompany(id);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                //log error
+                return StatusCode(500, ex.Message);
+            }
         }
     }
 }
